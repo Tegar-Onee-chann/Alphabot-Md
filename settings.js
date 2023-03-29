4bc524b07af28f747a0c0c28a1ebc0923188d3ac
Tegar-Onee-chann <76033665+Tegar-Onee-chann@users.noreply.github.com>
2023-03-30 00:13:09 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -40,18 +40,18 @@ global.language = indonesia //change indonesia to english if you don't understan
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername = "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
-global.myweb = "https://s.id/zeeoneofc" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/@ZeeoneOfc" //bebas asal jan hapus
-global.myweb2 = "https://s.id/zeeoneofc" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
+global.botname = "Zurrbot-Mdོ" //namabot kalian
+global.ownername = "ᴹᴿ᭄ ZurroneOfcོ ×፝֟͜×" //nama kalian
+global.myweb = "https://s.id/zurroneofc" //bebas asal jan hapus
+global.youtube = "https://youtube.com/c/@ZurrEdzt" //bebas asal jan hapus
+global.myweb2 = "https://s.id/zurroneofc" //bebas
+global.email = "zurrtegar@gmail.com" //bebas
 global.region = "Indonesia" //bebas
 global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["62887435047326", "62821931157232"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+"+ownernomer //nmr wa kalian
+global.premium = ['6281211037493'] //premium user
+global.owner = ["628121107493", "6281211037493"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "628121107493" // nomor wa kalian
+global.ownernomerr = "628121107493"+ownernomer //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome = "https://telegra.ph/file/edca4b513ddf29fd52c0e.jpg" // maks size 30kb, agar welcome image nya tdk delay
